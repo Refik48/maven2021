2bf74485fd6c3df29afe8dcca767dd7aba4e4ffd
Kazım Nihat Tekiner <nihattekiner@gmail.com>
2021-06-02 23:27:15 +0300

    update

---
+++ src/test/java/Day09_TestNG/C99_Sozel.java
@@ -0,0 +1,16 @@
+package Day09_TestNG;
+
+public class C99_Sozel {
+    /*
+        - TestNG tester'lara daha fazla kontrol imkani verir ve testleri daha etkili yapmamizi saglar.
+        - Tester'lar TestNG'yi etkili bir framework tasarlamak ve test case'leri TestNG annotation'lari
+        ile organize etmek icin kullanirlar.
+        - TestNG ile daha fazla before ve after method'lari kullanilabilir.
+        - Test caseleri siralama ozelligi (priority) ve test caselerin birbirine
+        bagimliligi (dependsOnMethod) bize testleri organize etmekte yardim eder.
+        - Yazilan test case'lerin paralel olarak calistirilmasina, birden fazla kullanilmasin imkan tanir.
+        - Error mesajlarinin daha detayli bir sekilde gosterilmesini saglar.
+        - Multi-Browser Test
+        - Kullanisli HTML ve xml raporlari olusturmaya yarar.
+     */
+}
